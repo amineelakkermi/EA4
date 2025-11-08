077cda2433a48a5837d43ffa3e94cb63a9e83ac4
amineelakkermi <amineelakkermi37@gmail.com>
2025-11-08 21:51:15 +0100

    mise a jour

--- app/dashboard/ProjectList.tsx
+++ app/dashboard/ProjectList.tsx
@@ -12,8 +12,10 @@ const ProjectList = async () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
       {projects.map((project) => (
-        <ProjectCard key={project._id} project={project} />
+        <ProjectCard key={project._id || project.title}  {...project} />
       ))}
+
+      
     </div>
   )
 }
