516914856b8eb0d6262b23d6299d645db3ff409a
amineelakkermi <amineelakkermi37@gmail.com>
2025-11-13 16:52:36 +0100

    mise a jour

--- app/dashboard/edit/[id]/page.tsx
+++ app/dashboard/edit/[id]/page.tsx
@@ -1,18 +1,15 @@
 // app/dashboard/edit/[id]/page.tsx
-import { Suspense } from 'react'
 import UpdateProject from '../../UpdateProject'
 
 
 export default function EditProjectByIdPage({ params }: { params: { id: string } }) {
   const { id } = params
+
   return (
-   <Suspense fallback={<div>Loading...</div>}>
-     <section className="relative isolate min-h-screen flex flex-col items-center justify-center bg-white px-4 md:px-10">
+    <section className="relative isolate min-h-screen flex flex-col items-center justify-center bg-white px-4 md:px-10">
       <div className="z-10 w-full max-w-4xl p-6 rounded-2xl shadow-lg bg-white/90 backdrop-blur-md">
         <UpdateProject id={id} />
       </div>
     </section>
-
-   </Suspense>
   )
 }
